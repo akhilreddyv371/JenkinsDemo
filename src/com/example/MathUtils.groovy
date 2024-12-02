48f12a4047c9659718d44552c54f1e92b873109a
Akhil Reddy V <88768951+akhilreddyv371@users.noreply.github.com>
2024-12-02 15:51:17 +0530

    Create MathUtils.groovy

---
+++ src/com/example/MathUtils.groovy
@@ -0,0 +1,12 @@
+// src/com/example/MathUtils.groovy
+package com.example
+
+class MathUtils {
+    static int add(int a, int b) {
+        return a + b
+    }
+
+    static int multiply(int a, int b) {
+        return a * b
+    }
+}
